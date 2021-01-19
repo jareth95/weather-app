91c5665f8062408cb650bcf6db1fdf3dcfbc12b8
jareth95 <jareth_smith@hotmail.com>
2021-01-19 20:09:21 +0000

    added forecast

--- app.js
+++ app.js
@@ -9,8 +9,25 @@ window.addEventListener('load', () => {
     const temperatureSpan = document.querySelector('.temperature span')
     const searchElement = document.querySelector('[data-city-search]')
     const searchBox = new google.maps.places.SearchBox(searchElement)
+    const day1Forecast = document.querySelector('.day1')
+    const day2Forecast = document.querySelector('.day2')
+    const day3Forecast = document.querySelector('.day3')
+    const day4Forecast = document.querySelector('.day4')
+    const day1Temperature = document.querySelector('.temperature-degree1')
+    const day2Temperature = document.querySelector('.temperature-degree2')
+    const day3Temperature = document.querySelector('.temperature-degree3')
+    const day4Temperature = document.querySelector('.temperature-degree4')
+    const day1Description = document.querySelector('.temperature-description1')
+    const day2Description = document.querySelector('.temperature-description2')
+    const day3Description = document.querySelector('.temperature-description3')
+    const day4Description = document.querySelector('.temperature-description4')
+    const day1Icon = document.querySelector('.icon1')
+    const day2Icon = document.querySelector('.icon2')
+    const day3Icon = document.querySelector('.icon3')
+    const day4Icon = document.querySelector('.icon4')
     
-    let api 
+    let currentApi 
+    let forecastApi
     let long
     let lat
 
@@ -20,33 +37,36 @@ window.addEventListener('load', () => {
             long = position.coords.longitude
             lat = position.coords.latitude
 
-            // Use commented section to add future forecast
-            // api = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&appid=09d7593febfa1a230b21516c97cb70d8`
+            forecastApi = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&appid=09d7593febfa1a230b21516c97cb70d8`
 
-            api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=09d7593febfa1a230b21516c97cb70d8`
-
-            fetch(api)
+            fetch(forecastApi)
             .then(response => {
                 return response.json();
             })
             .then(data => {
                 console.log(data)
-                settingDOMElements(data)
+                settingForecastDOMElements(data)
+            })
+
+            currentApi = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&lang=en&appid=09d7593febfa1a230b21516c97cb70d8`
+
+            fetch(currentApi)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data)
+                settingCurrentDOMElements(data)
             })
         })
     }
     // setting DOM Elements
-    function settingDOMElements(data) {
+    function settingCurrentDOMElements(data) {
 
         const temp = data.main.temp
         const summary = data.weather[0].description
         const timezone = data.name
         const iconID = data.weather[0].icon
-
-        // const {temp} = data.current
-        // const summary = data.current.weather[0].description
-        // const timezone = data.timezone
-        // const iconID = data.current.weather[0].icon
 
         temperatureDegree.textContent = Math.round(temp)
         temperatureDescription.textContent = summary
@@ -60,6 +80,67 @@ window.addEventListener('load', () => {
             body.classList.add('hot-background')
             body.classList.remove('cold-background')
         }
+    }
+    function settingForecastDOMElements(data) {
+        
+        switch (new Date().getDay()) {
+            case 0:
+              day1Forecast.textContent = "Monday";
+              day2Forecast.textContent = "Tuesday";
+              day3Forecast.textContent = "Wednesday";
+              day4Forecast.textContent = "Thursday";
+              break;
+            case 1:
+              day1Forecast.textContent = "Tuesday";
+              day2Forecast.textContent = "Wednesday";
+              day3Forecast.textContent = "Thursday";
+              day4Forecast.textContent = "Friday";
+              break;
+            case 2:
+              day1Forecast.textContent = "Wednesday";
+              day2Forecast.textContent = "Thursday";
+              day3Forecast.textContent = "Friday";
+              day4Forecast.textContent = "Saturday";
+              break;
+            case 3:
+              day1Forecast.textContent = "Thursday";
+              day2Forecast.textContent = "Friday";
+              day3Forecast.textContent = "Saturday";
+              day4Forecast.textContent = "Sunday";
+              break;
+            case 4: 
+              day1Forecast.textContent = "Friday";
+              day2Forecast.textContent = "Saturday";
+              day3Forecast.textContent = "Sunday";
+              day4Forecast.textContent = "Monday";
+              break;
+            case 5: 
+              day1Forecast.textContent = "Saturday";
+              day2Forecast.textContent = "Sunday";
+              day3Forecast.textContent = "Monday";
+              day4Forecast.textContent = "Tuesday";
+              break;
+            case 6:
+              day1Forecast.textContent = "Sunday";
+              day2Forecast.textContent = "Monday";
+              day3Forecast.textContent = "Tuesday";
+              day4Forecast.textContent = "Wednesday";
+          }
+
+          day1Temperature.innerHTML = Math.round(data.daily[1].temp.day)
+          day2Temperature.innerHTML = Math.round(data.daily[2].temp.day)
+          day3Temperature.innerHTML = Math.round(data.daily[3].temp.day)
+          day4Temperature.innerHTML = Math.round(data.daily[4].temp.day)
+
+          day1Description.innerHTML = data.daily[1].weather[0].description
+          day2Description.innerHTML = data.daily[2].weather[0].description
+          day3Description.innerHTML = data.daily[3].weather[0].description
+          day4Description.innerHTML = data.daily[4].weather[0].description
+
+          day1Icon.src = `/images/${data.daily[1].weather[0].icon}.png`
+          day2Icon.src = `/images/${data.daily[2].weather[0].icon}.png`
+          day3Icon.src = `/images/${data.daily[3].weather[0].icon}.png`
+          day4Icon.src = `/images/${data.daily[4].weather[0].icon}.png`
     }
     // Switch between celcius & farenheit
     temperatureSection.addEventListener('click', () => {
@@ -87,16 +168,27 @@ window.addEventListener('load', () => {
         if (place == null) return
         lat = place.geometry.location.lat()
         long = place.geometry.location.lng()
-        // api = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&appid=09d7593febfa1a230b21516c97cb70d8`
-        api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=09d7593febfa1a230b21516c97cb70d8`
 
-            fetch(api)
+        forecastApi = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&appid=09d7593febfa1a230b21516c97cb70d8`
+
+        fetch(forecastApi)
             .then(response => {
                 return response.json();
             })
             .then(data => {
                 console.log(data)
-                settingDOMElements(data)
+                settingForecastDOMElements(data)
+            })
+        
+        currentApi = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&lang=en&appid=09d7593febfa1a230b21516c97cb70d8`
+
+            fetch(currentApi)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data)
+                settingCurrentDOMElements(data)
             })
     })
 })
